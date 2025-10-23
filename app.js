f3be8a87d145f872e14fd20bd5a7653ae301f819
Lavith Raj <189830163+lraj23@users.noreply.github.com>
2025-10-22 20:11:17 -0700

    added /ymbactive-join-channel to join the private channel

--- app.js
+++ app.js
@@ -7,8 +7,28 @@ const headers = {
 const lraj23BotTestingId = "C09GR27104V";
 const lraj23UserId = "U0947SL6AKB";
 const iWillBuryYouAliveInADarkAlleyAndLetTheRatsFeastUponYourCorpse = "i-will-bury-you-alive-in-a-dark-alley-and-let-the-rats-feast-upon-your-corpse";
+const YMBActiveChannelId = "C09MUE0M5GA";
 
 app.message("", async ({ message }) => { });
+
+app.command("/ymbactive-join-channel", async interaction => {
+	await interaction.ack();
+	const userId = interaction.body.user_id;
+	let joined = false;
+	try {
+		await app.client.conversations.invite({
+			channel: YMBActiveChannelId,
+			users: interaction.body.user_id
+		});
+		joined = true;
+	} catch (e) {
+		console.error(e.data.error);
+	}
+	if (joined) await app.client.chat.postMessage({
+		channel: YMBActiveChannelId,
+		text: "<@" + userId + "> has joined <#" + YMBActiveChannelId + ">! Let's see how long they stay..."
+	});
+});
 
 app.message(/secret button/i, async ({ message: { channel, user, thread_ts, ts } }) => await app.client.chat.postEphemeral({
 	channel, user,
